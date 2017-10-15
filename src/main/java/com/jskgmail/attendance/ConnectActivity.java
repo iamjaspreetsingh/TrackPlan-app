22ec78946e54412961738e132cf5e0d6429cd1d8
iamjaspreetsingh <jsk1961998@gmail.com>
2017-10-16 01:44:39 +0530

    update

--- src/main/java/com/jskgmail/attendance/ConnectActivity.java
+++ src/main/java/com/jskgmail/attendance/ConnectActivity.java
@@ -2,12 +2,19 @@ package com.jskgmail.attendance;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 public class ConnectActivity extends AppCompatActivity {
-  static   String mynaam="",usernamee;
+  static   String mynaam="",usernamee="fd";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,13 +23,57 @@ public class ConnectActivity extends AppCompatActivity {
         final EditText name=(EditText)findViewById(R.id.editText3);
         final EditText username=(EditText)findViewById(R.id.editText4);
         Button button=(Button)findViewById(R.id.button7) ;
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        final DatabaseReference myRef = database.getReference("Name");
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 mynaam=name.getText().toString();
                 usernamee=username.getText().toString();
+
+Log.d("djfdjfdfjdfn",usernamee);
+                myRef.setValue(usernamee);
+
+
+                myRef.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        // This method is called once with the initial value and again
+                        // whenever data at this location is updated.
+                        String value = dataSnapshot.getValue(String.class);
+                        Log.d("gogogo", "Value is: " + value);
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError error) {
+                        // Failed to read value
+                        Log.w("gogogo", "Failed to read value.", error.toException());
+                    }
+                });
+
+
+
             }
         });
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+    private void go() {
+
+
+
+
+
 
     }
 }
