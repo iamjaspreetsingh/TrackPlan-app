ce4fe4f0a13e6fed68407f7bcbbb70b8e9603a16
iamjaspreetsingh <jsk1961998@gmail.com>
2017-10-17 00:15:38 +0530

    update

---
+++ src/main/java/com/jskgmail/attendance/ListViewAdaptersea.java
@@ -0,0 +1,125 @@
+package com.jskgmail.attendance;
+
+/**
+ * Created by JASPREET SINGH on 16-10-2017.
+ */
+
+import android.app.Activity;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by JASPREET SINGH on 06-08-2017.
+ */
+
+public class ListViewAdaptersea extends BaseAdapter {
+    Activity mcontext;
+    ArrayList<String> title;
+    ArrayList<String> description;
+String textt="";
+
+    public ListViewAdaptersea(SearchActivity context, ArrayList<String> arrayList, ArrayList<String> arrayList1) {
+        mcontext=context;
+        title=arrayList;
+        description=arrayList1;
+    }
+
+    public ListViewAdaptersea(String text, SearchActivity context, ArrayList<String> arrayList, ArrayList<String> arrayList1)
+    {
+
+        mcontext=context;
+        title=arrayList;
+        description=arrayList1;
+        textt=text;
+        Log.v("sosososo",textt);
+
+
+    }
+    @Override
+    public int getCount() {
+
+        return title.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    public class ViewHolder{
+        TextView txtviewtitle;
+        TextView txtviewdesc;
+
+
+    }
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder holder;
+
+
+        LayoutInflater inflater=mcontext.getLayoutInflater();
+        if(convertView==null)
+        {
+            convertView=inflater.inflate(R.layout.allfriend,null);
+            holder=new ViewHolder();
+            holder.txtviewtitle=(TextView)convertView.findViewById(R.id.textView48);
+
+
+            holder.txtviewdesc=(TextView)convertView.findViewById(R.id.textView49);
+            holder.txtviewdesc.setVisibility(View.VISIBLE);
+            holder.txtviewtitle.setVisibility(View.VISIBLE);
+
+
+            if(!(textt.equals("")))
+            { if((title.get(position).toLowerCase().contains(textt))||(description.get(position).toLowerCase().contains(textt))) {
+                holder.txtviewdesc.setVisibility(View.VISIBLE);
+                holder.txtviewtitle.setVisibility(View.VISIBLE);
+
+                holder.txtviewtitle.setText(title.get(position));
+                holder.txtviewdesc.setText(description.get(position));
+            }
+            else {
+                holder.txtviewdesc.setVisibility(View.GONE);
+            holder.txtviewtitle.setVisibility(View.GONE);
+            }
+            }
+            else {
+                holder.txtviewtitle.setText(title.get(position));
+                holder.txtviewdesc.setText(description.get(position));
+            }
+
+
+
+
+
+
+
+
+
+        }
+        else{
+            holder=(ViewHolder)convertView.getTag();
+        }
+
+
+
+
+
+        return convertView;
+    }
+
+
+
+}
+
--- src/main/java/com/jskgmail/attendance/SearchActivity.java
+++ src/main/java/com/jskgmail/attendance/SearchActivity.java
@@ -1,13 +1,97 @@
 package com.jskgmail.attendance;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.SearchView;
 
-public class SearchActivity extends AppCompatActivity {
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
+
+public class SearchActivity extends AppCompatActivity implements SearchView.OnQueryTextListener {
+ListView list;
+    ListViewAdaptersea adapter;
+    SearchView search;
+    String[] names;
+    ArrayList<String> arrayList=new ArrayList<>();
+    ArrayList<String> arrayList1=new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+        names=new String[]{"ab","dbd","ffddf","gfgf"
+        };
+        list=(ListView)findViewById(R.id.listsearch);
+
+
+
+
+
+
+        final FirebaseDatabase database = FirebaseDatabase.getInstance();
+        final DatabaseReference myRef = database.getReference("user");
+
+        myRef.addValueEventListener(new ValueEventListener() {
+
+
+
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                for (DataSnapshot dataSnapshot1 : dataSnapshot.getChildren()) {
+
+
+                        Log.d("soso", "" + dataSnapshot1.getKey());
+                    Log.d("sosooo", "" + dataSnapshot1.child("name").getValue());
+                    arrayList.add("" + dataSnapshot1.getKey());
+arrayList1.add(" : " + dataSnapshot1.child("name").getValue());
+
+
+                }
+
+
+            } @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+                                        }
+                                    });
+
+
+        adapter=new ListViewAdaptersea(this,arrayList,arrayList1);
+        list.setAdapter(adapter);
+        search= (SearchView) findViewById(R.id.searchView);
+        search.setOnQueryTextListener(this);
+
+list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        Log.d("yoyoy",arrayList.get(position));
+    }
+});
+
+    }
+
+
+    @Override
+    public boolean onQueryTextSubmit(String query) {
+        return true;
+    }
+
+    @Override
+    public boolean onQueryTextChange(String newText) {
+        String text=newText;
+
+        adapter=new ListViewAdaptersea(text,this,arrayList,arrayList1);
+      list.setAdapter(adapter);
+        return false;
     }
 }
